3faad2022f1ae060a8640fd06b47c812fc1d951a
sami16kazah <samkazah444@gmail.com>
2024-11-12 19:30:33 +0100

    change config server

--- config/server.js
+++ config/server.js
@@ -1,9 +1,11 @@
+
 module.exports = ({ env }) => ({
-  host: env('RENDER_EXTERNAL_HOSTNAME', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: '0.0.0.0', // Allows Render to handle the IP assignment
+  port: env.int('PORT', 1337), // Render sets this automatically
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS'), // Ensure APP_KEYS is set
   },
+  url: `https://${env('RENDER_EXTERNAL_HOSTNAME')}`, // Public URL if required for your app setup
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
