cad2257f4e15cf2b6bc9830ef174ae0850e9ab6d
sami16kazah <samkazah444@gmail.com>
2024-11-12 19:25:18 +0100

    fix news-letter

--- app/api/news-letter/route.ts
+++ app/api/news-letter/route.ts
@@ -10,15 +10,14 @@ export async function POST(request: Request) {
   }
 
   try {
-    const response = await axios.post(
-      `${process.env.NEXT_PUBLIC_API_URL}/news-letters`,
-      {
-        data: {
-          email,
-        },
-      }
-    );
-    return NextResponse.json({ message: `Congratulations! You have successfully subscribed to our newsletter.` });
+    await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/news-letters`, {
+      data: {
+        email,
+      },
+    });
+    return NextResponse.json({
+      message: `Congratulations! You have successfully subscribed to our newsletter.`,
+    });
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (error: any) {
     return NextResponse.json(
