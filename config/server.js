fd8d6ed895a7ac0f09140c45657cbf6c9e1296cd
sami16kazah <samkazah444@gmail.com>
2024-11-13 14:50:36 +0100

    server restore app keys

--- config/server.js
+++ config/server.js
@@ -1,6 +1,9 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"), // Default to 0.0.0.0 if HOST is not set
   port: env.int("PORT", 1337),   // Default to 1337 if PORT is not set
+  app: {
+    keys: env.array("APP_KEYS", ["defaultKey"]), // Ensure APP_KEYS is an array
+  },
   webhooks: {
     populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
