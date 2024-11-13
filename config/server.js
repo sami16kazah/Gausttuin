7db236eb3e1102baf0c4b7526e80d5c2d15dc31e
sami16kazah <samkazah444@gmail.com>
2024-11-13 14:07:06 +0100

    server 2

--- config/server.js
+++ config/server.js
@@ -1,7 +1,6 @@
 module.exports = ({ env }) => ({
   host: env("HOST"), // Allows Render to handle the IP assignment
-  port: env.int("PORT"), // Render sets this automatically
-  proxy: true,
+  port: env.int("PORT"), // Render sets this automaticall
      url: env('RENDER_EXTERNAL_HOSTNAME'), // Sets the public URL of the application.
   app: {
     keys: env.array("APP_KEYS"), // Ensure APP_KEYS is set
