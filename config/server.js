8719944ed3924b8ce64dd5affccf8dcce12a1df0
sami16kazah <samkazah444@gmail.com>
2024-11-13 14:00:44 +0100

    change server config

--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env("HOST"), // Allows Render to handle the IP assignment
   port: env.int("PORT"), // Render sets this automatically
   proxy: true,
-     url: env('API_URL'), // Sets the public URL of the application.
+     url: env('RENDER_EXTERNAL_HOSTNAME'), // Sets the public URL of the application.
   app: {
     keys: env.array("APP_KEYS"), // Ensure APP_KEYS is set
   },
