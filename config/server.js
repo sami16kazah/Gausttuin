1924e4d3fd487ed68c1cc698f41bd55653792b2d
sami16kazah <samkazah444@gmail.com>
2024-11-13 13:58:35 +0100

    change server config

--- config/server.js
+++ config/server.js
@@ -1,18 +1,12 @@
 module.exports = ({ env }) => ({
-  host: env('HOST'), // Allows Render to handle the IP assignment
+  host: env("HOST"), // Allows Render to handle the IP assignment
   port: env.int("PORT"), // Render sets this automatically
+  proxy: true,
+     url: env('API_URL'), // Sets the public URL of the application.
   app: {
     keys: env.array("APP_KEYS"), // Ensure APP_KEYS is set
   },
-  url: `https://${env("RENDER_EXTERNAL_HOSTNAME")}`, 
   webhooks: {
     populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
-  },
-  admin: {
-    // ...
-    path: '/admin',
-    build: {
-      backend: env('ADMIN_BUILD_BACKEND', 'https://gastuin.onrender.com'),
-    },
   },
 });
