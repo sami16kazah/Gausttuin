d43bd3772b82c90cc83bbf24b89956f344380f35
sami16kazah <samkazah444@gmail.com>
2024-11-15 13:42:13 +0100

    change email config

--- config/plugins.js
+++ config/plugins.js
@@ -1,31 +1,29 @@
 module.exports = ({ env }) => ({
   email: {
-    provider: 'mailjet',
-    providerOptions: {
-      publicApiKey: env('MAILJET_PUBLIC_KEY'),
-      secretApiKey: env('MAILJET_SECRET_KEY'),
-    },
-    settings: {
-      defaultFrom:  env('MAILJET_SENDER_EMAIL'),
-      defaultReplyTo: env('MAILJET_SENDER_EMAIL'),
-    },
-  },
-    upload: {
-      config: {
-        provider: 'cloudinary',
-        providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
-        },
-        actionOptions: {
-          upload: {},
-          delete: {},
-        },
+    config: {
+      provider: "strapi-provider-email-mailjet",
+      providerOptions: {
+        publicApiKey: env("MAILJET_API_KEY"),
+        secretApiKey: env("MAILJET_SECRET_KEY"),
+      },
+      settings: {
+        defaultFrom: env("MAILJET_SENDER_EMAIL"),
+        defaultFromName: env("MAILJET_SENDER_EMAIL"),
       },
     },
-  }
-
-
-);
-  
+  },
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
+});
