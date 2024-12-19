d70df62b80e61a0d367eb039a0021910738f89ed
sami16kazah <samkazah444@gmail.com>
2024-12-19 23:07:33 +0100

    remove the mesaure internet

--- components/pages/ClientWrapper.tsx
+++ components/pages/ClientWrapper.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 import LoadingModal from "../LoadingModal";
-import { getInternetSpeed } from "@/util/getInternetSpeed";
+
 
 export default function ClientWrapper({
   children,
@@ -14,18 +14,11 @@ export default function ClientWrapper({
   const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
-    let timeout = 1500; // Default timeout in milliseconds
+    const timeout = 1500; // Default timeout in milliseconds
 
     // Dynamically adjust the timeout based on internet speed
     const measureSpeedAndSetLoading = async () => {
       setLoading(true);
-
-      const speed = await getInternetSpeed();
-
-      // Adjust timeout: Faster speed = shorter delay, Slower speed = longer delay
-      if (speed > 5000000) timeout = 1000; // Fast network (5 Mbps+)
-      else if (speed > 2000000) timeout = 1500; // Moderate network (2-5 Mbps)
-      else timeout = 2000; // Slow network (<2 Mbps)
 
       const timer = setTimeout(() => setLoading(false), timeout);
 
